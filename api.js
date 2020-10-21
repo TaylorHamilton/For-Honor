1bed026daf5f0dc2091a779e4de535c445994091
Taylor Hamilton <thamilton162@west-mec.org>
2020-10-21 13:47:25 -0700

    test

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const data = null;
+const data = document.getElementById('player');
 
 const xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
@@ -8,7 +8,7 @@ xhr.addEventListener("readystatechange", function () {
 		console.log(this.responseText);
 	}
 });
-
+data.innerHTML = data.
 xhr.open("GET", "https://rapidapi.p.rapidapi.com/leaderboard/1/battle");
 xhr.setRequestHeader("x-rapidapi-host", "call-of-duty-modern-warfare.p.rapidapi.com");
 xhr.setRequestHeader("x-rapidapi-key", "2cdcc9a70emsh3e5379ce087c538p19c704jsn52b0efd2bd99");
