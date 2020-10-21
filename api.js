680cd4b44e1601274f897ec1671ef7b5ebd42779
Samuel <sharris163@west-mec.org>
2020-10-21 13:40:17 -0700

    added leaderboard

--- api.js
+++ api.js
@@ -1,25 +1,16 @@
-const player = document.getElementById('player')
-////////Create our Request to server
-let xhr = new XMLHttpRequest();
-////////Set up a callback function to run when server req is successful///////////
-xhr.onreadystatechange = () => {
-    ///////Create an if statement that asks if the server req is complete/////////////
-    if (xhr.readyState === 4)
-        ///////now server is complete create an if to see if connection succcessful///////
-        if (xhr.status = 200) {
-            ///////Body of the callback where we parse data and use DOM Manipulation//////////
-            const data = JSON.parse(xhr.response);
-            console.log(data);
-            player.innerHTML = data.el.attributes;
-          
-            ///////Else if clause where we throw an error if we dont have success connecting//
-        } else if (xhr.status === 404) {
-            alert("Oh no the page wasn't found");
-        } else if (xhr.status === 500) {
-            alert("Awhh man the server is taking a break :(");
-        }
-};
-///////open server request////////////////////////////////////////////////////////
-xhr.open("GET", "https://tracker.gg/for-honor/leaderboards/stats/all/Wins");
-///////send server request////////////////////////////////////////////////////////
-xhr.send();
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open("GET", "https://rapidapi.p.rapidapi.com/leaderboard/1/battle");
+xhr.setRequestHeader("x-rapidapi-host", "call-of-duty-modern-warfare.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "2cdcc9a70emsh3e5379ce087c538p19c704jsn52b0efd2bd99");
+
+xhr.send(data);
