19ab5e42f9863143314ca45ec3464440c88b039a
Taylor <zodd515@gmail.com>
2020-10-22 15:12:07 -0700

    trying to get it to work

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const data = document.getElementById('player');
+
 const xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 
@@ -8,34 +8,7 @@ xhr.addEventListener("readystatechange", function () {
         sendHttpRequest('GET', "https://rapidapi.p.rapidapi.com/leaderboard/1/battle");
     }
 });
-<<<<<<< HEAD
-const sendHttpRequest = (method, url) => {
-	const promise = new Promise((resolve, reject) => {
-		console.log(url);
-		const xhr = new XMLHttpRequest();
-		xhr.open(method, url);
-		xhr.onload = () => {
-			///////////// When browser load parse http req to usable format//////////
-			const data = JSON.parse(xhr.response);
-			console.log(data);
 
-			document.getElementById('player').innerHTML = data.entries[0].username;
-
-			xhr.open("GET", "https://rapidapi.p.rapidapi.com/leaderboard/1/battle");
-			xhr.setRequestHeader("x-rapidapi-host", "call-of-duty-modern-warfare.p.rapidapi.com");
-			xhr.setRequestHeader("x-rapidapi-key", "5c57e915c7msh50e80a6f3111130p12ab1djsnee726d84ca19");
-
-			xhr.send(data);
-		}
-	})
-    
-};
-=======
-
-const getData = () => {
-    var answer = document.getElementById("").value;
-    console.log(answer);
-};
 
 const sendHttpRequest = (method, url) => {
     const promise = new Promise((resolve, reject) => {
@@ -47,12 +20,9 @@ const sendHttpRequest = (method, url) => {
             console.log(data);
 
             document.getElementById('leaderboard').src = data.sprites.front_default;
-            var str = "" + data.id;
-            var pad = "000";
-            var ans = pad.substring( data.length - str.length) + str;
-            document.getElementById('info_Player').value = "#" + ans + " " + data.name;
-            document.getElementById('info_Kills').value = data.number[0];
-            document.getElementById('info_Wins').value = data.number[0];
+            document.getElementById('Player').innerHTML = data.entries[x].username;
+            document.getElementById('Kills').innerHTML = data.number[0];
+            document.getElementById('Wins').innerHTML = data.number[0];
         }
     })
 };
@@ -60,4 +30,4 @@ xhr.open("GET", "https://rapidapi.p.rapidapi.com/leaderboard/1/battle");
 xhr.setRequestHeader("x-rapidapi-host", "call-of-duty-modern-warfare.p.rapidapi.com");
 xhr.setRequestHeader("x-rapidapi-key", "2cdcc9a70emsh3e5379ce087c538p19c704jsn52b0efd2bd99");
 xhr.send(data);
->>>>>>> master
+
