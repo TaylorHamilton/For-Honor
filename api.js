e38ddcad2564cecaba2727269a9954ac13f6dcd8
Taylor <zodd515@gmail.com>
2020-10-27 14:00:35 -0700

    Auto stash before merge of "master" and "origin/master"

--- api.js
+++ api.js
@@ -1,10 +1,11 @@
 const getBtn = document.querySelector('.btn');
 const player = document.getElementById("Player");
-const getData = () =>{
-    var answer = document.getElementById('search').value;
-    console.log(answer);
-    var leaderboard ="https://rapidapi.p.rapidapi.com/leaderboard/1/" + answer;
-    sendHttpRequest('GET', leaderboard);
+const tr = document.querySelector('tr')
+const getData = () => {
+	var answer = document.getElementById('search').value;
+	console.log(answer);
+	var leaderboard = "https://rapidapi.p.rapidapi.com/leaderboard/1/" + answer;
+	sendHttpRequest('GET', leaderboard);
 };
 
 const sendHttpRequest = (method, url) => {
@@ -16,16 +17,19 @@ const sendHttpRequest = (method, url) => {
 		xhr.onload = () => {
 			const data = JSON.parse(xhr.response);
 			console.log(data);
-
-			player.textContent = data.entries[0].username;
-			document.getElementById('Kills').innerHTML = data.number[0];
-			document.getElementById('Wins').innerHTML = data.entries[0].wins;
+			for (var y = 0; y < tr.length; y++) {
+				for (var x = 0; x < data.entries.length; x++) {
+					player.textContent = data.entries[x].username;
+					document.getElementById('Kills').innerHTML = data.entries[x].values.kills;
+					document.getElementById('Wins').innerHTML = data.entries[x].values.wins;
+				}
+			}
 		}
-
+		xhr.setRequestHeader("x-rapidapi-host", "call-of-duty-modern-warfare.p.rapidapi.com");
+		xhr.setRequestHeader("x-rapidapi-key", "2cdcc9a70emsh3e5379ce087c538p19c704jsn52b0efd2bd99");
+		xhr.send();
 	})
-	xhr.setRequestHeader("x-rapidapi-host", "call-of-duty-modern-warfare.p.rapidapi.com");
-	xhr.setRequestHeader("x-rapidapi-key", "2cdcc9a70emsh3e5379ce087c538p19c704jsn52b0efd2bd99");
-	xhr.send();
+
 
 };
 
